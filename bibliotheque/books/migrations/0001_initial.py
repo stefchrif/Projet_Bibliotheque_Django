53674036a9ab76818ef7275197e9f40c22838312
stefchrif <mustapha.elat@gmail.com>
2026-03-15 17:50:08 +0000

    Fix logout 405 by using POST logout form

---
+++ bibliotheque/books/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+    initial = True
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=120, unique=True)),
+                ('description', models.TextField(blank=True)),
+            ],
+            options={'verbose_name': 'Catégorie', 'verbose_name_plural': 'Catégories', 'ordering': ['name']},
+        ),
+        migrations.CreateModel(
+            name='Book',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=255)),
+                ('author', models.CharField(max_length=255)),
+                ('isbn', models.CharField(max_length=13, unique=True)),
+                ('publication_year', models.PositiveIntegerField(blank=True, null=True)),
+                ('total_copies', models.PositiveIntegerField(default=1)),
+                ('available_copies', models.PositiveIntegerField(default=1)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('category', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='books', to='books.category')),
+            ],
+            options={'ordering': ['title']},
+        ),
+    ]
